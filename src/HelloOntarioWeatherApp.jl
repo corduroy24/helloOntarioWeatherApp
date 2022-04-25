79640470d022271b2a7b69d837249c0bb196ce91
Korede <korede.adegboye@hotmail.com>
2022-04-25 05:25:20 -0400

    remove icon

--- src/HelloOntarioWeatherApp.jl
+++ src/HelloOntarioWeatherApp.jl
@@ -12,3 +12,21 @@ function main()
 end
 
 end
+
+
+function launchServer(port)
+
+  Genie.config.run_as_server = true
+  Genie.config.server_host = "0.0.0.0"
+  Genie.config.server_port = port
+
+  println("port set to $(port)")
+
+  route("/") do
+      "Hi there!"
+  end
+
+  Genie.AppServer.startup()
+end
+
+launchServer(parse(Int, ARGS[1]))
